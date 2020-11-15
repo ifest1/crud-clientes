673d226d10cfb733edbf16250d2d395c3768e889
iago <iago_filipe@protonmail.com>
2020-11-15 18:34:10 -0300

    setando heroku

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
